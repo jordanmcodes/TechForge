49be800d6ac9d0a5d6234ab29ce5e364650027ff
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-11 13:22:48 +0100

    Added prodcut model to models.py

--- products/models.py
+++ products/models.py
@@ -8,3 +8,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True,on_delete=models.SET_NULL)
