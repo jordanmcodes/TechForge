c46f6cfc7ed89655ef70aef3298759c541d05b8b
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-15 10:07:27 +0100

    Created products view in views.py in the products file.

--- products/views.py
+++ products/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 
 from .models import Product
 # Create your views here.
+
+def all_products(request):
+    """ a view to showcase all products """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
