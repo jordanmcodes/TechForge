4288261e119d70990f4b7b5763b85a9a41d8aef2
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-19 09:31:25 +0100

    created a view so that the application returns the basket page.

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_basket(request):
+    """ A view to return the basket page """
+
+    return render(request, 'basket/basket.html')
