f77de7bb5d88d35d5991e2b4d903092ac4041b5d
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-19 19:12:54 +0100

    added qunatity select so that users can select multiple products.

--- basket/views.py
+++ basket/views.py
@@ -6,3 +6,7 @@ def view_basket(request):
     """ A view to return the basket page """
 
     return render(request, 'basket/basket.html')
+
+def add_to_basket(request, item_id):
+        """add quantity of selected product to the basket"""
+        pass
