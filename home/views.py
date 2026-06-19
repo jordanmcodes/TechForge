112976348b7bab4b9e556798198e7a281ac989de
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-19 12:27:58 +0100

    Added homepage quieries to ensure product images will appear on the homepage straight from the database.

--- home/views.py
+++ home/views.py
@@ -4,4 +4,22 @@ from products.models import Product
 # Create your views here.
 
 def index(request):
-    return render (request, 'home/index.html')
+    desktop_products = Product.objects.filter(
+        category__name='desktop_pc'
+    ) [:1]
+
+    laptop_products = Product.objects.filter(
+        category__name='laptops'
+    ) [:1]
+
+    mobile_products = Product.objects.filter(
+        category__name='mobile_devices'
+    ) [:1]
+    
+    context = {
+        'desktop_products': desktop_products,
+        'laptop_products': laptop_products,
+        'mobile_products': mobile_products,
+    }
+
+    return render (request, 'home/index.html', context)
