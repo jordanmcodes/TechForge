964789ba615b5172cb27ba5d9242be63dbda56bf
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-19 23:10:25 +0100

    checkout and stripe is setup.

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from .forms import OrderForm
+from django.conf import settings
 
 def checkout(request):
     basket = request.session.get('basket', {})
@@ -10,8 +11,12 @@ def checkout(request):
     
     order_form = OrderForm()
     template = 'checkout/checkout.html'
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    client_secret = 'test client secret'
     context = {
         'order_form': order_form,
+        'stripe_public_key':stripe_public_key,
+        'client_secret': client_secret
     }
 
     return render(request, template, context)
