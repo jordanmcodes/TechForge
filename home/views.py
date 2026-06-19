eccc86b196067afb793d2553bc11c61547619c8d
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-19 13:19:41 +0100

    Put the [1] back as they are needed for the slideshow as it should only show one image from each category.

--- home/views.py
+++ home/views.py
@@ -6,15 +6,15 @@ from products.models import Product
 def index(request):
     desktop_products = Product.objects.filter(
         category__name='desktop_pc'
-    )
+    ) [:1]
 
     laptop_products = Product.objects.filter(
         category__name='laptops'
-    ) 
+    ) [:1]
 
     mobile_products = Product.objects.filter(
         category__name='mobile_devices'
-    ) 
+    ) [:1]
     
     context = {
         'desktop_products': desktop_products,
