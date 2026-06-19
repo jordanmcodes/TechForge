6489f77d614a5152ab6d24b83a4e7ed51bedc2f2
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-19 13:42:42 +0100

    Added secondary group so that images also will appear on the right block.

--- home/views.py
+++ home/views.py
@@ -16,7 +16,19 @@ def index(request):
     mobile_products = Product.objects.filter(
         category__name='mobile_devices'
     ) [:1]
+
+    tablet_products = Product.objects.filter(
+        category__name='tablets'
+    ) [:1]
     
+    console_products = Product.objects.filter(
+        category__name='gaming_consoles'
+    ) [:1]
+
+    accessory_products = Product.objects.filter(
+        category__name='accessories'
+    ) [:1]
+
     featured_products = list(
         chain(
            desktop_products,
@@ -24,11 +36,21 @@ def index(request):
            mobile_products, 
         )
     )
+
+    featured_products_right = list(
+        chain(
+           tablet_products,
+           console_products,
+           accessory_products, 
+        )
+    )
     context = {
         'desktop_products': desktop_products,
         'laptop_products': laptop_products,
         'mobile_products': mobile_products,
         'featured_products': featured_products,
+        'featured_products_right': featured_products_right,
+
     }
 
     return render (request, 'home/index.html', context)
