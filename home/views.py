44adae2163694031727a875a521aec7c1f214a44
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-19 13:27:00 +0100

    Added featured homepage products to the context section on views.py.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from products.models import Product
+from itertools import chain 
 
 # Create your views here.
 
@@ -16,10 +17,18 @@ def index(request):
         category__name='mobile_devices'
     ) [:1]
     
+    featured_products = list(
+        chain(
+           desktop_products,
+           laptop_products,
+           mobile_products, 
+        )
+    )
     context = {
         'desktop_products': desktop_products,
         'laptop_products': laptop_products,
         'mobile_products': mobile_products,
+        'featured_products': featured_products,
     }
 
     return render (request, 'home/index.html', context)
