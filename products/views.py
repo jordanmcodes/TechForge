4a9e6df517130feb37d61c74727427f601f0c2f8
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-19 09:15:33 +0100

    added category filter for the views.py file.

--- products/views.py
+++ products/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Product
+from .models import Product, Category
 # Create your views here.
 
 def all_products(request):
     """ a view to showcase all products """
 
     products = Product.objects.all()
+    current_categories = None
+
+    if 'category' in request.GET:
+        categories = request.GET['category'].split(',')
+
+        products = products.filter(
+            category__name__in=categories)
+        
+        current_categories = Category.objects.filter(
+            name__in=categories)
 
     context = {
         'products': products,
+        'current_categories': current_categories,
     }
     return render(request, 'products/products.html', context)
 
