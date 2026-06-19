9eb74bb8ffd27e38785998d3180345b737dee5a4
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-19 09:40:02 +0100

    Fixed typo in path basket code.

--- TechForge/urls.py
+++ TechForge/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
-    path('/basket', include('basket.urls')),
+    path('basket/', include('basket.urls')),
 
 ]
 
