9d46e886dcef7333fc9d022c50263bba66e55fe5
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-20 08:39:48 +0100

    fixed static not working.

--- TechForge/settings.py
+++ TechForge/settings.py
@@ -55,12 +55,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'TechForge.urls'
@@ -166,8 +168,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS =[
-    BASE_DIR /'static',
+    BASE_DIR /'static',   
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles',
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
