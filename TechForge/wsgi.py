88d48ef01de563a44554404b85461810f0143fb8
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-20 10:52:30 +0100

    fixed wsgi.py crashing the deployment by having lowcase techforge instead of uppercase.

--- TechForge/wsgi.py
+++ TechForge/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techforge.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechForge.settings')
 
 application = get_wsgi_application()
