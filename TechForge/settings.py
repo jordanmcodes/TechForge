617e4b12fb740735eb82c698f3d2721950449d09
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-23 12:30:39 +0100

    added '127.0.0.1' and local host to allowed hosts.

--- TechForge/settings.py
+++ TechForge/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["techforge-877818eefd36.herokuapp.com"]
+ALLOWED_HOSTS = ["techforge-877818eefd36.herokuapp.com",
+                 "'127.0.0.1'",
+                 "localhost",]
 
 
 # Application definition
