d688a2a950eca8ffa7cb2dea3cfb3c36e91cd323
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-23 12:18:41 +0100

    changed it back to techforge instead of TechForge.

--- TechForge/asgi.py
+++ TechForge/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechForge.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techforge.settings')
 
 application = get_asgi_application()
