9f7bb6413cdf24bbd696330c9eb4f41b4a2a168b
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-25 01:41:55 +0100

    fixed typo in webhooks.py

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -25,7 +25,7 @@ def webhook(request):
     handler = StripeWH_Handler(request)
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent_payment_failed': handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
     }
 
     event_handler = event_map.get(event['type'], handler.handle_event)
