4cf85847a8d6e6616bf9385f173ff4695c90f4a9
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-06-27 11:11:49 +0100

    Adjusted checkout form so only phone numbers can be entered

--- checkout/forms.py
+++ checkout/forms.py
@@ -37,3 +37,4 @@ class OrderForm(forms.ModelForm):
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
+            self.fields['phone_number'].widget.attrs['pattern'] = '[0-9+ ]+'
